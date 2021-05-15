befb3cc46058770be9071efbcda2a6f9be944a88
Mahany <mahmoud.mahany1@gmail.com>
2021-05-15 18:45:26 +0200

    A

---
+++ Modules.py
@@ -0,0 +1,16 @@
+#################################################
+#----------------Built-In Modules---------------#
+#################################################
+
+# import random
+# print(f"Print Random Float Number: {random.random()}")
+
+# # Show all functions inside module
+# import random
+# print(dir(random))
+
+# Import some functions from module
+from random import randint
+print(f"Print Random Integer: {randint(100, 200)}")
+
+#---------------------------------------------------------------------------------------------------------------------------------------------#
